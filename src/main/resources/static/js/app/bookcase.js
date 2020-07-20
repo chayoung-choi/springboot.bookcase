37ae270d7dd9fb6dc4d65ccad88a0cb185199f7a
chayoung <chayoung.dev@gmail.com>
2020-07-20 23:40:09 +0900

    tab layout swiper 적용 #3

--- src/main/resources/static/js/app/bookcase.js
+++ src/main/resources/static/js/app/bookcase.js
@@ -6,3 +6,18 @@ var bookcase = {
 };
 
 bookcase.init();
+
+
+$(document).ready(function () {
+	var swiper = new Swiper('.swiper-container', {
+	    speed: 400,
+	    spaceBetween: 100
+	});
+	
+	swiper.on('slideChange', function () {
+	  console.log('slide changed', swiper.activeIndex);
+	  var tabIndex = swiper.activeIndex + 1;
+	  var target = document.querySelector('.nav-link[tabindex="'+ tabIndex +'"]');
+	  tab.toggleActiveTab(target);
+	});
+});
--- src/main/resources/static/js/app/common.js
+++ src/main/resources/static/js/app/common.js
@@ -9,16 +9,6 @@ var common = {
 			$("#" + btnId).click();
 		}
 	},
-	// layout nav tab click event toggle
-	toggleNavTab : function(parentName, targetName){
-		var target = event.target;
-		if (target.closest(targetName) == null){
-			return;
-		}
-		var navbar = target.closest(parentName);
-		navbar.querySelector(targetName + '.active').classList.remove('active');
-		target.closest(targetName).classList.add('active');
-	},
 	// fa-chevron-up/down 아이콘 toggle
 	toggleChevronIcon : function() {
 		var target = event.target.querySelector('.icon-chevron i');
@@ -31,10 +21,43 @@ var common = {
 			target.classList.add('fa-chevron-down');
 		}
 	},
+
 	// 숫자 세자리씩 포맷 
 	numberCommaFormat : (n) => {
 		return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 	} 
 };
 
+var tab = {
+	init : function() {
+		var _this = this;
+	},
+	moveNavTab : () => {
+		var target = event.target;
+		if (target.closest('.nav-item') == null){
+			return;
+		}
+		
+//		var prevTabIndex = event.currentTarget.querySelector('.nav-link.active').tabIndex;
+		
+		tab.toggleActiveTab(target);
+		tab.slideSwiper(target);
+	},
+	toggleActiveTab : (target) => {
+		var nav = target.closest('.nav');
+		nav.querySelector('.nav-link.active').classList.remove('active');
+		target.closest('.nav-link').classList.add('active');
+	},
+	setLocation : () => {
+		
+	},
+	slideSwiper : (target) => {
+		var mySwiper = document.querySelector('.swiper-container').swiper;
+		if (mySwiper == null){ return; }
+		var tabIndex = target.closest('.nav-link').tabIndex - 1;
+		mySwiper.slideTo(tabIndex);
+	}
+}
+
 common.init();
+tab.init();
